08624298cd82072ffad17efa7fa10198f8ac69a7
amit1kadam <140226590+amit1kadam@users.noreply.github.com>
2023-08-07 14:42:59 +0530

    Update TestBase.java

--- src/main/java/com/yatra/qa/base/TestBase.java
+++ src/main/java/com/yatra/qa/base/TestBase.java
@@ -23,6 +23,10 @@ public class TestBase {
 	public static Properties prop;
 	public static EventFiringWebDriver e_driver;
 	public static WebEventListener eventListener;
+	public  String dCity = "Pune";
+	public String aCity = "Chennai";
+	public  String tDate = "28/08/2023";
+public TestBase() {	
 		try {
 			prop = new Properties();
 			FileInputStream ip = new FileInputStream(
@@ -41,11 +45,25 @@ public class TestBase {
 	public void initialization()     {
 		String browserName = prop.getProperty("browser");
 			WebDriverManager.chromedriver().setup();
-			ChromeOptions opt = new ChromeOptions();
-			opt.addArguments("disable-notifications");
-			opt.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
-			driver = new ChromeDriver(opt);
+			
 		}
+
+		if(browserName.equals("chrome")){
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions opt = new ChromeOptions();
+				opt.addArguments("disable-notifications");
+				opt.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+				driver = new ChromeDriver(opt);
+				 // driver = new ChromeDriver();
+				  
+				  
+				//System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
+				//driver = new ChromeDriver(); 
+			}
+			else if(browserName.equals("FF")){
+				System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
+				driver = new FirefoxDriver(); 
+			}
 
 		
 		e_driver = new EventFiringWebDriver(driver);
