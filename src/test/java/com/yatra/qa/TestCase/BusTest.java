91ff76ccf43f0c4d110dcb61b01997cc6f236e3d
Harshad Wani <Harshad Wani@DESKTOP-2O18T4K>
2023-08-10 22:09:15 +0530

    Please Check and review code.

--- src/test/java/com/yatra/qa/TestCase/BusTest.java
+++ src/test/java/com/yatra/qa/TestCase/BusTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 import com.yatra.qa.base.TestBase;
 import com.yatra.qa.pages.BusPage;
-import com.yatra.qa.pages.LoginPage;
+//import com.yatra.qa.pages.LoginPage;
 import com.yatra.qa.util.TestUtil;
 
 public class BusTest extends TestBase{
